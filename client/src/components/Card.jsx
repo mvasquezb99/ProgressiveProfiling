d95d40d0ff6001264c8d3daa572e9c628fd084c5
Esteban Muriel <emurielr@eafit.edu.co>
2025-02-15 16:10:45 -0500

    fix: Card styles fixed

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,9 @@
 export default function Card({ rem = 25, children }) {
   return (
-    <div className={`bg-[#f4f4fa] flex flex-col items-center w-[${rem}rem] p-8 rounded-xl shadow-lg`}>
+    <div
+      className={`bg-[#f4f4fa] flex flex-col items-center p-8 rounded-xl shadow-lg`}
+      style={{ width: `${rem}rem` }}
+    >
       {children}
     </div>
   );
