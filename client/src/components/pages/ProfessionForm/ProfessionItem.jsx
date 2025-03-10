041d7cdcf2b1f97914402509014813af03eb30eb
Esteban Muriel <emurielr@eafit.edu.co>
2025-03-10 17:59:39 -0500

    fix: fixed some professionForm styles

--- client/src/components/pages/ProfessionForm/ProfessionItem.jsx
+++ client/src/components/pages/ProfessionForm/ProfessionItem.jsx
@@ -7,7 +7,7 @@ export default function ProfessionItem({ profession, handleClick }) {
   const [isClicked, setIsClicked] = useState(false);
 
   const itemClass =
-    'border-2 border-gray-200 rounded-xl text-center flex items-center justify-center p-4 shadow-md hover:shadow-lg hover:cursor-pointer ';
+    'border-2 border-gray-200 rounded-xl text-center flex items-center max-h-40 justify-center p-4 shadow-md hover:shadow-lg hover:cursor-pointer ';
 
   const toggleClass = () => {
     if (isClicked) {
@@ -18,7 +18,7 @@ export default function ProfessionItem({ profession, handleClick }) {
   };
   return (
     <div
-      className={isClicked ? itemClass : itemClass + 'bg-gray-70' + ' bg-white hover:bg-gray-50'}
+      className={isClicked ? itemClass : itemClass + 'bg-gray-70 ' + ' bg-white hover:bg-gray-50 '}
       onClick={() => {
         toggleClass();
         handleClick(profession);
