9727f3fb0470b04edc1cb9f121ba2189e9c4b6bb
VillegasMich <manuel.villegas.michel@gmail.com>
2025-04-26 17:09:34 -0500

    feat: posthog initialization

--- demo/src/app/layout.js
+++ demo/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { PostHogProvider } from "../components/PostHogProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,13 +19,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* Navbar */}
-        {children}
-        {/* Footer */}
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <PostHogProvider>
+          {/* Navbar */}
+          {children}
+          {/* Footer */}
+        </PostHogProvider>
       </body>
     </html>
   );
