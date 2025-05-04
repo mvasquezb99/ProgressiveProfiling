1cbbd01c12db198f847db62bbbc9ac57000362be
VillegasMich <manuel.villegas.michel@gmail.com>
2025-05-04 17:19:56 -0500

    feat: cron job and endpoint to update user profiles given posthog events

--- server/src/posthog/posthog.module.ts
+++ server/src/posthog/posthog.module.ts
@@ -3,9 +3,10 @@ import { PosthogService } from './posthog.service';
 import { HttpModule } from '@nestjs/axios';
 import { PosthogController } from './posthog.controller';
 import { UserModule } from 'src/user/user.module';
+import { QueryModule } from 'src/query/query.module';
 
 @Module({
-  imports: [HttpModule, UserModule],
+  imports: [HttpModule, UserModule, QueryModule],
   providers: [PosthogService],
   controllers: [PosthogController],
 })
--- server/src/posthog/posthog.service.ts
+++ server/src/posthog/posthog.service.ts
@@ -4,6 +4,11 @@ import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { PosthogEventsResponseDto } from './dto/posthog-evet-response.dto';
 import { AxiosResponse } from 'axios';
+import { UserService } from 'src/user/user.service';
+import { QueryNode, QueryService } from 'src/query/query.service';
+import { OccupationPropertiesI } from 'src/occupation/occupation.model';
+import { UserClass } from 'src/user/user.model';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class PosthogService {
@@ -13,6 +18,9 @@ export class PosthogService {
   constructor(
     private readonly configService: ConfigService,
     private readonly httpService: HttpService,
+    private readonly userService: UserService,
+    private readonly userClass: UserClass,
+    private readonly queryService: QueryService,
   ) {
     this.apiKey = this.configService.getOrThrow<string>('POSTHOG_API_KEY');
     this.projectId =
@@ -55,13 +63,87 @@ export class PosthogService {
     return response.data;
   }
 
+  async getAllJobOfferEventsLast24h(): Promise<PosthogEventsResponseDto> {
+    const after24h = new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString();
+    const response$ = this.httpService.get<PosthogEventsResponseDto>(
+      `https://us.posthog.com/api/projects/${this.projectId}/events/`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`,
+        },
+        params: {
+          event: this.eventName,
+          after: after24h,
+        },
+      },
+    );
+    const response: AxiosResponse<PosthogEventsResponseDto> =
+      await firstValueFrom(response$);
+    console.log(
+      'Number of job offer events in the last 24h:',
+      response.data.results.length,
+    );
+    return response.data;
+  }
+
+  @Cron('0 0 * * *') // Every day at midnight
   async getAllJobOfferEventsAndUpdateUsers() {
-    const events = await this.getAllJobOfferEvents();
-    events.results.forEach((event) => {
-      console.log('User identifier:', event.distinct_id);
-      console.log('Job Offer name:', event.properties.name);
-      console.log('Category name:', event.properties.category);
-      console.log('Occupations:', event.properties.occupations);
-    });
+    const events = await this.getAllJobOfferEventsLast24h();
+    let numUsersUpdated = 0;
+
+    for (const event of events.results) {
+      const userEmail = event.distinct_id;
+      // const jobOfferName = event.properties.name;
+      // const jobOfferCategory = event.properties.category;
+      const jobOfferOccupations = event.properties.occupations;
+
+      try {
+        const user = await this.userService.findByEmail(userEmail);
+        if (!user) {
+          console.error(`Can't identify user with ${userEmail}`);
+          continue;
+        }
+        const dislikedOccupations = new Array<string>();
+        const currentUserOccupaitons = new Array<string>();
+
+        user.occupations.forEach((occupation) => {
+          currentUserOccupaitons.push(occupation.name);
+        });
+
+        const relationsDislikedNodes =
+          await this.queryService.getDislikedOccupationsFromUser(user.name);
+        const occupationsDislikedQueryNodes =
+          relationsDislikedNodes.records.map((r) => r.get('o') as QueryNode);
+        occupationsDislikedQueryNodes.forEach((node) => {
+          const props = node.properties as OccupationPropertiesI;
+          dislikedOccupations.push(props.name);
+        });
+
+        for (const occupation of jobOfferOccupations) {
+          if (
+            !currentUserOccupaitons.includes(occupation) &&
+            !dislikedOccupations.includes(occupation)
+          ) {
+            await this.userClass.userModel.relateTo({
+              alias: 'LikesOccupation',
+              where: {
+                source: {
+                  name: user.name,
+                },
+                target: {
+                  name: occupation,
+                },
+              },
+            });
+            numUsersUpdated += 1;
+          }
+        }
+      } catch (err) {
+        console.error(`Can't identify user with ${userEmail} - ${err}`);
+        continue;
+      }
+    }
+    console.log(`${numUsersUpdated} users occupations updated`);
+    return `${numUsersUpdated} users occupations updated`;
   }
 }
